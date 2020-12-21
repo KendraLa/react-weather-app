489891ce1be1c8515105a4a8202bf716b6f6be8e
Kendra <kendralambillon@gmail.com>
2020-12-21 17:55:34 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Kendra Lambillon and is{" "}
+        <a href="https://github.com/KendraLa/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
