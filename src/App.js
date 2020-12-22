fbca6cf3347ce56f5d2e4af50d81878dc4ab2ba0
Kendra <kendralambillon@gmail.com>
2020-12-22 11:35:43 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Kendra Lambillon and is{" "}
-        <a href="https://github.com/KendraLa/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Kendra Lambillon and is{" "}
+          <a
+            href="https://github.com/KendraLa/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
