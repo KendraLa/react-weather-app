6681f29332626167618eba0ee623d7fefee40085
Kendra <kendralambillon@gmail.com>
2021-01-12 12:12:15 +0100

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,12 +16,14 @@ export default function WeatherForecast(props) {
   if (loaded && props.city === forecast.city.name) {
     return (
       <div className="WeatherForecast row">
-        <WeatherForecastPreview data={forecast.list[0]} />
-        <WeatherForecastPreview data={forecast.list[1]} />
-        <WeatherForecastPreview data={forecast.list[2]} />
-        <WeatherForecastPreview data={forecast.list[3]} />
-        <WeatherForecastPreview data={forecast.list[4]} />
-        <WeatherForecastPreview data={forecast.list[5]} />
+        <div className="col-2">
+          <WeatherForecastPreview data={forecast.list[0]} />
+          <WeatherForecastPreview data={forecast.list[1]} />
+          <WeatherForecastPreview data={forecast.list[2]} />
+          <WeatherForecastPreview data={forecast.list[3]} />
+          <WeatherForecastPreview data={forecast.list[4]} />
+          <WeatherForecastPreview data={forecast.list[5]} />
+        </div>
       </div>
     );
   } else {
